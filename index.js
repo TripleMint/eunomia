4bbc56996b5dfb83c7b4e9efee75211815f684cd
Alexander Schmitz <arschmitz@gmail.com>
2022-05-20 12:36:14 -0400

    I'm not working in football. Basically, I'm a father with two kids and taking some time for myself. I played for 25 years, and I believe I need some space for myself. -- Paolo Maldini

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const tsNode = require('ts-node');
 
 tsNode.register({
-    project: path.join(process.cwd(), 'tsconfig.json'),
+    project: path.join(__dirname, 'tsconfig.json'),
 });
 
 module.exports = {
